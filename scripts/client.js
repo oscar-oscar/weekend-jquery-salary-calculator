bbd1eb6110f2a4ff02fe2eee1692064f9dbf345b
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-07-26 01:48:31 -0500

    added addEmployee function to append employee's info to table upon filling fields and clicking submit

--- scripts/client.js
+++ scripts/client.js
@@ -1,9 +1,47 @@
 console.log('script sourced');
 //initialize 
 $(readyNow); 
+
+const monthlyBudget = 20000;
+
+let employees = [];
 /**
  * This function is called after the HTML has loaded.
  */
 function readyNow() {
     console.log('ready now');
+    //when submit button is clicked it runs addEmployee function
+    $( '#submitEmployeeButton' ).on( 'click', addEmployee );
 }
+
+//function that adds employye after fields are filled
+function addEmployee(){
+    console.log('in addEmployee');
+
+    //varible values for for inputs
+    let firstName = $( '#first-name-field' ).val();
+    let lastName =  $( '#last-name-field' ).val();
+    let numberId = $( '#id-number-field' ).val();
+    let title = $( '#job-title-field' ).val();
+    let salary = $( '#annual-salary-field' ).val();
+
+
+    
+//appending table upon fields being filled and submitted
+    $('#employee-info').append(`
+    <tr> 
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${numberId}</td>
+        <td>${title}</td>
+        <td>${salary}</td>
+    `)
+    //push info into employeees array
+    employees.push( addEmployee );
+}
+
+//function displayEmployee(){
+    //console.log('in displayEmployee');
+
+    
+    //el.append(newEmployee);
